2bdce8744cc5a4fa1d32c7fdc6534ae8344a1835
Jay <jay.veer1@protonmail.com>
2023-01-14 17:19:57 +0000

    Added scraper, json support

--- main.go
+++ main.go
@@ -1,72 +1,100 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
-	"log"
 	"os"
-
+    "io/ioutil"
+	"time"
+    "encoding/json"
+    "net/url"
 	"github.com/gocolly/colly"
-	"github.com/gocolly/redisstorage"
+	"strings"
+
 )
 
-// func main() {
+func joinUrl(urlToJoin string) string{
 
-// 	fName := "data.csv"
-//     file, err := os.Create(fName)
-//     if err != nil {
-//         log.Fatalf("Could not create file, err: %q", err)
-//         return
-//     }
-//     defer file.Close()
+	var base = "http://books.toscrape.com/catalogue/"
 
-//     writer := csv.NewWriter(file)
-//     defer writer.Flush()
-//     c := colly.NewCollector(
-//         colly.AllowedDomains("w3schools.com"),
-//     )
-// 	if p, err := proxy.RoundRobinProxySwitcher(
-// 		"socks5://127.0.0.1:1337",
-// 		"socks5://127.0.0.1:1338",
-// 		"http://127.0.0.1:8080",
-// 	); err == nil {
-// 		c.SetProxyFunc(p)
-// 	}
+	u, err := url.JoinPath(base, urlToJoin)
+	if err != nil {
+		os.Exit(1)
+	}
+	return u
+}
 
+
+type item struct {
+	BookTitle string `json:"Book Title"`
+	BookUrl string `json:"Book Url"`
+	BookPrice string `json:"Book Price"`
+	CrawledAt time.Time `json:"Crawled at Time"`
+}
 
 func main() {
-    fName := "data.csv"
-    file, err := os.Create(fName)
-    if err != nil {
-        log.Fatalf("Could not create file, err: %q", err)
-        return
-    }
-    defer file.Close()
+	stories := []item{}
+	joinUrl("base.html")
+	// Instantiate default collector
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+		colly.AllowedDomains("books.toscrape.com"),
+		colly.MaxDepth(2),
+		colly.Async(true),
+	)
 
-    writer := csv.NewWriter(file)
-    defer writer.Flush()
-		storage := &redisstorage.Storage{
-    Address:  "127.0.0.1:6379",
-    Password: "",
-    DB:       0,
-    Prefix:   "job01",
+	c.OnHTML("article.product_pod", func(e *colly.HTMLElement) {
+		temp := item{
+		BookTitle : e.ChildText("h3"),
+		BookPrice :e.ChildText("p.price_color"),
+		CrawledAt: time.Now(),
+		}
+		temp.BookUrl = joinUrl(e.ChildAttr("a", "href"))
+		stories = append(stories, temp)
+	})
+
+
+	// On every span tag with the class next-button
+	c.OnHTML("li.next", func(h *colly.HTMLElement) {
+		var pageUrl = h.ChildAttr("a", "href")
+		var baseUrl string
+	if strings.Contains(pageUrl, "catalogue") {
+		baseUrl = "http://books.toscrape.com/"
+	} else{
+		baseUrl = "http://books.toscrape.com/catalogue"
+	}
+		t,err :=url.JoinPath(baseUrl,pageUrl )
+		if err != nil {
+		os.Exit(1)
+	}
+		fmt.Println("Visiting",t)
+		c.Visit(t)
+	})
+
+	// Set max Parallelism and introduce a Random Delay
+	c.Limit(&colly.LimitRule{
+		Parallelism: 20,
+		RandomDelay: 5 * time.Second,
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+
+	})
+
+	// Crawl all pages the user passes in
+	scrape_pages := [2]string{"http://books.toscrape.com/"}
+	for _, scrape_page := range scrape_pages {
+		c.Visit(scrape_page)
+
 	}
 
-	err := c.SetStorage(storage)
-	if err != nil {
-		panic(err)
+	c.Wait()
+	data, err := json.MarshalIndent(stories, "", "  ")
+	if err!=nil{
+		fmt.Println(err)
 	}
+	ioutil.WriteFile("Books.json", data, 0644)
+	fmt.Println(stories)
 
-    c := colly.NewCollector()
-    c.OnHTML("table#customers", func(e *colly.HTMLElement) {
-        e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
-            writer.Write([]string{
-                el.ChildText("td:nth-child(1)"),
-                el.ChildText("td:nth-child(2)"),
-                el.ChildText("td:nth-child(3)"),
-            })
-        })
-        fmt.Println("Scraping Complete")
-    })
-    c.Visit("https://www.w3schools.com/html/html_tables.asp")
 }
